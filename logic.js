151e7ff7148daa92ddf0404c18925bb402ba4134
jac267 <jackpotchap@gmail.com>
2024-05-07 12:57:08 -0400

    automatic update

--- logic.js
+++ logic.js
@@ -1,14 +1,18 @@
 function start() {
   //slice 1 car le premier element est toujours null
   lsJSONChallenges = JSON.parse(localStorage["challengesData"]).slice(1);
-  const lsChallenges = []
-  for (JSONChallenge in lsJSONChallenges){
-    item = lsJSONChallenges[JSONChallenge]
-    lsChallenges.push(new Challenge(item["description"], item["points"], item["completed"]));
+  const lsChallenges = [];
+  for (JSONChallenge in lsJSONChallenges) {
+    item = lsJSONChallenges[JSONChallenge];
+    lsChallenges.push(
+      new Challenge(item["description"], item["points"], item["completed"])
+    );
   }
 
   container = new ChallengesContainer(
     lsChallenges,
     document.getElementById("container")
   );
+
+  container.update();
 }
